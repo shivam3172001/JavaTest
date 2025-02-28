a38ae0580cd35be610a369c3ec4eff0448d777b8
Shivam kumar choudhary <152508164+shivam3172001@users.noreply.github.com>
2025-02-28 10:30:43 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ class hello
     public static void main(String[] args) {
         System.out.println("Hello DevOps Engineers!");
         System.out.println("Testing with new line");
+        System.out.println("Code tested Successfully in Jenkins");
     }
 }
